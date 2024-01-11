1c54099494c2c95d15b12bc4f305bdb873a0a1fb
Theron Joe <theronj@joetwebdev.io>
2024-01-11 10:43:13 -0700

    comments for next steps on index and leaders table. wip

--- src/server/api/routers/leader.ts
+++ src/server/api/routers/leader.ts
@@ -8,6 +8,10 @@ export const leaderRouter = createTRPCRouter({
 	getList: publicProcedure.query(({ctx}) => {
 		return ctx.db.query.leaders.findMany({
 			limit: 10,
+			// each offset needs to append 10
+			// for pagination
+			// add search based on tribe, first and last name, biaAgency and state
+			offset: 10,
 		})
 	}),
 });
